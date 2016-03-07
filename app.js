cb7abf57c1ab982a335f12312eb988ad0005ac8d
Siva Tumma <siva.tumma@callhealth.co.in>
2016-03-07 15:55:26 +0530

    Test on azure

--- app.js
+++ app.js
@@ -12,73 +12,55 @@ var io = require('socket.io')(server);
 
 app.use(cors());
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+	res.header("Access-Control-Allow-Origin", "http://localhost");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
 });
 
 app.set('view engine', 'jade');
 app.use(express.static(path.join(process.cwd(), 'app')));
 
 app.get('/', function(req, res) {
-  res.sendfile(__dirname + 'dist/index.html');
+	res.sendfile(__dirname + 'dist/index.html');
 });
 
-io.on('connection', function(socket) {
-  console.log("Client connected through 'connect' socket.io ");
-
-  /**********************************************************************************************
-   ************   SPECIAL CODE THAT FETCHES DEVICE DATA - DON'T DISTURB UNLESS YOU KNOW - BELOW */
-
-
-/*
-var server = require('net').createServer(function (socket) {
-	socket.on('data',function(data){
-		console.log("Data from client",data, new Buffer(data,'hex').toString('utf8'));
-		
+var listening = false;
+var server = require('net').createServer(function(deviceSocket) {
+	deviceSocket.on('data', function(data) {
+		var decodedDeviceData = new Buffer(data, 'hex').toString('utf8');
+		if(decodedDeviceData.startsWith('!1')){
+			//	Implies first connection
+		}
+		console.log("Data from client", decodedDeviceData);
+		doPost(decodedDeviceData);
 	});
 });
 
-*/
-
-  var listening = false;
-  var server = require('net').createServer(function(deviceSocket) {
-    deviceSocket.on('data', function(data) {
-      var decodedDeviceData = new Buffer(data,'hex').toString('utf8');
-      console.log("Data from client", decodedDeviceData);
-	doPost(decodedDeviceData);
-	//console.log("This is the socket.io socket ", socket);
-      //socket.emit('data',{data:data});
-      // deviceSocket.emit('data', decodedDeviceData);
-
-    });
-  });
-
-
-  // server.on('connection', function(c) {    console.log("Connected a device");  });
-  server.on('listening', function() {    listening = true;  });
-  // server.on('data', function(d) {    console.log("Here is d", d);  });
-  // server.on('end', function() {    console.log("This client is disconnecting, disconnected");  });
-
-  if(!listening) server.listen(5062, "0.0.0.0");
-
-  /**************************************************************************************
-   *******   SPECIAL CODE THAT FETCHES DEVICE DATA - DON'T DISTURB UNLESS YOU KNOW ABOVE */
+server.on('connection', function(c) {    console.log("Connected a device",c);  });
+server.on('listening', function() {
+	listening = true;
 });
+// server.on('data', function(d) {    console.log("Here is d", d);  });
+// server.on('end', function() {    console.log("This client is disconnecting, disconnected");  });
+
+if (!listening) server.listen(5062, "0.0.0.0");
+
+/**************************************************************************************
+ *******   SPECIAL CODE THAT FETCHES DEVICE DATA - DON'T DISTURB UNLESS YOU KNOW ABOVE */
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-  extended: false
+	extended: false
 }));
 app.use(cookieParser());
 
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
 
@@ -124,25 +106,25 @@ function doPost(data) {
 
 	req.write(JSON.stringify(postData));
 	req.end();
-}// will print stacktrace
+} // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
+	app.use(function(err, req, res, next) {
+		res.status(err.status || 500);
+		res.render('error', {
+			message: err.message,
+			error: err
+		});
+	});
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+	res.status(err.status || 500);
+	res.render('error', {
+		message: err.message,
+		error: {}
+	});
 });
 
 server.listen(80);
